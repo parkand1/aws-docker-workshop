8721423ba0fedc369f0a3bd5cc98df3aa47fa278
peter <peter.mckee@gmail.com>
2020-10-26 18:23:44 -0500

    Update server to use mongo and scale with compose file

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const ronin 		= require( 'ronin-server' )
 const mocks 		= require( 'ronin-mocks' )
+const database	= require( 'ronin-database' )
 
 async function main() {
 
 	try {
+		const dbConnectionString = process.env.DB_CONN_STR || 'localhost:27017/crud-datastore'
+		console.log( `dbConnectionString: ${dbConnectionString}` )
+		
+		await database.connect( process.env.DB_CONN_STR )
 
     const server = ronin.server({
 			port: process.env.PORT || 8080
 		})
 
-		server.use( '/services/', mocks.server( server.Router(), false, true ) )
+		server.use( '/services/', mocks.server( server.Router() ) )
 
     const result = await server.start()
     console.info( result )
